3d4e41c07c75fb3d83f45924a53dacaf7e190ab5
Scripted Logic <149482797+DancingPumpkin65@users.noreply.github.com>
2024-06-03 13:41:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'includes/PHP/config.php'; ?>
+    <?php include 'includes/PHP/need.php'; ?>
+    <?php include 'includes/PHP/nav.php'; ?>
+    <?php include 'includes/PHP/hero.php'; ?>
+    <?php include 'includes/PHP/counter.php'; ?>
+    <?php include 'includes/PHP/offers.php'; ?>
+    <?php include 'includes/PHP/avis.php'; ?>
+    <?php include 'includes/PHP/faq.php'; ?>
+    <?php include 'includes/PHP/footer.php'; ?>
+</body>
+</html>
